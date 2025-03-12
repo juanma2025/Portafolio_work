e6e66bad517911966987afbf9569d1a619c5cc38
Juan Manuel cabrera <127572806+juanma2025@users.noreply.github.com>
2025-03-12 08:22:55 -0500

    portafolio

--- portfolio/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-  return (
-    <main className="flex itmens-center justify-center w-screen h-screen bg-cyan-300">
-      <h1 className="text-cyan-700 text-4xl font-extrabold">Juan Manuel</h1>
-      
-    </main>
-  );
-}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center w-screen h-screen bg-cyan-300">
+      <h1 className="text-cyan-700 text-4xl font-bold">Juan Manuel</h1>
+      
+    </main>
+  );
+}
