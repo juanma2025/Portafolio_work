9ee02dfe9888158ae1c3b4e10d87cba019433e34
Juan Manuel cabrera <127572806+juanma2025@users.noreply.github.com>
2025-03-15 14:39:30 -0500

    cambio en el page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center w-screen h-screen bg-cyan-300">
-      <h1 className="text-cyan-700 text-4xl font-bold">Juan Manuel</h1>
+    <main className="flex items-center justify-center w-screen h-screen bg-indigo-500 ">
+      <section className="bg-indigo-300 rounded-2xl shadow-xl w-2/4 p-5">
+      <h1 className="text-gray-950 text-4xl font-extrabold text-center">Juan Manuel</h1>
+      </section>
+    
       
     </main>
   );
