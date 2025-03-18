5f77670447b2abc9252acf55cebe1409ba3f1787
Juan Manuel cabrera <127572806+juanma2025@users.noreply.github.com>
2025-03-18 00:19:59 -0500

    cambios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center w-screen h-screen bg-indigo-500 ">
-      <section className="bg-indigo-300 rounded-2xl shadow-xl w-2/4 p-5">
-      <h1 className="text-gray-950 text-4xl font-extrabold text-center">Juan Manuel</h1>
+    <main className="flex items-center justify-center w-screen h-screen bg-[var(--color-background )] ">
+      <section className="bg-[var(--color-secondary)] rounded-2xl shadow-xl w-2/4 p-5">
+      <h1 className="bg-[var(--color-text-primary)] text-4xl font-extrabold text-center">Juan Manuel</h1>
       </section>
     
       
